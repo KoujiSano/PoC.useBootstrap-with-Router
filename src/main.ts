797ff3f194e11d8b584429384f76198cc9a0e986
佐野浩司 ( Kouji Sano ) <KoujiSano@users.noreply.github.com>
2022-04-03 19:09:25 +0900

    router

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.css";
 import { useBootstrap } from "vue-usebootstrap";
+import router from './router'
 
-createApp(App).use(useBootstrap, { prefix: "Bv" }).mount('#app')
+createApp(App).use(router).use(useBootstrap, { prefix: "Bv" }).mount('#app')
