cb1548566029c794c96913e14f9f3e8bf55da0f2
KoujiSano <sano@simplise.jp>
2022-04-03 20:36:40 +0900

    7 files modified, 2 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
-
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">
+      Home
+    </router-link> |
+    <router-link to="/about">
+      About
+    </router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,94 @@
 <template>
-  <BvNavbar expand="lg" theme="dark" background-color="primary">
+  <BvNavbar
+    expand="lg"
+    theme="dark"
+    background-color="primary"
+  >
     <BvContainer type="fluid">
       <BvNavbarBrand>Navbar</BvNavbarBrand>
-      <BvNavbarToggler></BvNavbarToggler>
+      <BvNavbarToggler />
       <BvNavbarCollapse>
         <BvNavbarNavList margin="e-auto b-2 b-lg-0">
           <BvNavItem>
-            <BvNavLink active>Home</BvNavLink>
+            <BvNavLink active>
+              Home
+            </BvNavLink>
           </BvNavItem>
           <BvNavItem>
             <BvNavLink>Link</BvNavLink>
           </BvNavItem>
           <BvNavItem>
-            <BvNavLink disabled>Disabled</BvNavLink>
+            <BvNavLink disabled>
+              Disabled
+            </BvNavLink>
           </BvNavItem>
         </BvNavbarNavList>
-        <BvForm flex role="search">
-          <BvFormInput margin="e-2" type="search" placeholder="Search"></BvFormInput>
-          <BvButton button="outline-success" type="submit">Search</BvButton>
+        <BvForm
+          flex
+          role="search"
+        >
+          <BvFormInput
+            margin="e-2"
+            type="search"
+            placeholder="Search"
+          />
+          <BvButton
+            button="outline-success"
+            type="submit"
+          >
+            Search
+          </BvButton>
         </BvForm>
       </BvNavbarCollapse>
     </BvContainer>
   </BvNavbar>
-  <BvNavbar sticky shadow expand="lg" theme="light" background-color="light" style="--bs-bg-opacity: .5;">
+  <BvNavbar
+    sticky
+    shadow
+    expand="lg"
+    theme="light"
+    background-color="light"
+    style="--bs-bg-opacity: 0.5"
+  >
     <BvContainer type="fluid">
-          <BvButton
-      button="primary"
-      toggle="collapse"
-      target="#collapseWidthExample"
-    >Button with data-bs-target</BvButton>
+      <BvButton
+        button="primary"
+        toggle="collapse"
+        target="#collapseWidthExample"
+      >
+        Button with data-bs-target
+      </BvButton>
     </BvContainer>
   </BvNavbar>
   <BvDiv flex>
-    <BvCollapse sticky horizontal id="collapseWidthExample" style="width: 240px;">
+    <BvCollapse
+      id="collapseWidthExample"
+      sticky
+      horizontal
+      style="width: 240px"
+    >
       <BvCard>
-        <BvCardBody>This is some placeholder content for a horizontal collapse. It's hidden by default and shown when triggered.</BvCardBody>
+        <BvCardBody>
+          This is some placeholder content for a horizontal collapse. It's
+          hidden by default and shown when triggered.
+        </BvCardBody>
       </BvCard>
     </BvCollapse>
-    <BvDiv :paddin="2" flex-grow="grow-1" style="height:1000px;">Content</BvDiv>
+    <BvDiv
+      :paddin="2"
+      flex-grow="grow-1"
+      style="height: 1000px"
+    >
+      Content
+    </BvDiv>
   </BvDiv>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '../components/HelloWorld.vue' 
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-  }
+  components: {},
 })
 </script>
